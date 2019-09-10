082563dee11b3fe25bd09d05b5c141d44c1e5f41
Dawn McNeil <dawn.a.mcneil@gmail.com>
2019-09-10 19:00:09 -0400

    update for publishing to production

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const helmet = require('helmet');
 const app = express();
 
-
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 app.use(function validateBearerToken(req,res,next){
@@ -49,8 +49,20 @@ function getMovies(req,res){
 
 app.get('/movies', getMovies);
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+    let response;
+    if(process.env.NODE_ENV === 'production'){
+        response = {error: {message: 'server error'}}
+    }
+    else{
+        response = {error}
+    }
+
+    res.status(500).json(response);
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () =>{
-    console.log(`Server listening on PORT ${PORT}`);
+    
 })
